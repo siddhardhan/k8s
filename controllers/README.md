ba2d429986bd6ae0dfda15f2762d13ebb15e344f
Siddhardha Nandipati <siddhardhan@gmail.com>
2018-11-27 23:17:10 -0500

    controllers

---
+++ controllers/README.md
@@ -0,0 +1,3 @@
+# k8s controllers
+https://kubernetes.io/docs/reference/access-authn-authz/admission-controllers/  
+https://banzaicloud.com/blog/k8s-admission-webhooks/  
---
+++ pkg/webhook/validating/webhook.go
@@ -0,0 +1,130 @@
+package validating
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+
+	"github.com/siddhardhan/k8s/pkg/log"
+	"github.com/siddhardhan/k8s/pkg/observability/metrics"
+	"github.com/siddhardhan/k8s/pkg/webhook"
+	"github.com/siddhardhan/k8s/pkg/webhook/internal/helpers"
+	"github.com/siddhardhan/k8s/pkg/webhook/internal/instrumenting"
+)
+
+// WebhookConfig is the Validating webhook configuration.
+type WebhookConfig struct {
+	Name string
+	Obj  metav1.Object
+}
+
+func (c *WebhookConfig) validate() error {
+	errs := ""
+
+	if c.Name == "" {
+		errs = errs + "name can't be empty"
+	}
+
+	if c.Obj == nil {
+		errs = errs + "; obj can't be nil"
+	}
+
+	if errs != "" {
+		return fmt.Errorf("invalid configuration: %s", errs)
+	}
+
+	return nil
+}
+
+// NewWebhook is a validating webhook and will return a webhook ready for a type of resource
+// it will validate the received resources.
+func NewWebhook(cfg WebhookConfig, validator Validator, ot opentracing.Tracer, recorder metrics.Recorder, logger log.Logger) (webhook.Webhook, error) {
+	if err := cfg.validate(); err != nil {
+		return nil, err
+	}
+
+	if logger == nil {
+		logger = log.Dummy
+	}
+
+	if recorder == nil {
+		logger.Warningf("no metrics recorder active")
+		recorder = metrics.Dummy
+	}
+
+	if ot == nil {
+		logger.Warningf("no tracer active")
+		ot = &opentracing.NoopTracer{}
+	}
+
+	// Create a custom deserializer for the received admission review request.
+	runtimeScheme := runtime.NewScheme()
+	codecs := serializer.NewCodecFactory(runtimeScheme)
+
+	// Create our webhook and wrap for instrumentation (metrics and tracing).
+	return &instrumenting.Webhook{
+		Webhook: &staticWebhook{
+			objType:      helpers.GetK8sObjType(cfg.Obj),
+			deserializer: codecs.UniversalDeserializer(),
+			validator:    validator,
+			cfg:          cfg,
+			logger:       logger,
+		},
+		ReviewKind:      metrics.ValidatingReviewKind,
+		WebhookName:     cfg.Name,
+		MetricsRecorder: recorder,
+		Tracer:          ot,
+	}, nil
+}
+
+// staticWebhook it's a validating webhook implementation for a  specific statuc object type.
+type staticWebhook struct {
+	objType      reflect.Type
+	deserializer runtime.Decoder
+	validator    Validator
+	cfg          WebhookConfig
+	logger       log.Logger
+}
+
+func (w *staticWebhook) Review(ctx context.Context, ar *admissionv1beta1.AdmissionReview) *admissionv1beta1.AdmissionResponse {
+	w.logger.Debugf("reviewing request %s, named: %s/%s", ar.Request.UID, ar.Request.Namespace, ar.Request.Name)
+
+	obj := helpers.NewK8sObj(w.objType)
+	runtimeObj, ok := obj.(runtime.Object)
+	if !ok {
+		err := fmt.Errorf("could not type assert metav1.Object to runtime.Object")
+		return w.toAdmissionErrorResponse(ar, err)
+	}
+
+	// Get the object.
+	_, _, err := w.deserializer.Decode(ar.Request.Object.Raw, nil, runtimeObj)
+	if err != nil {
+		err = fmt.Errorf("error deseralizing request raw object: %s", err)
+		return w.toAdmissionErrorResponse(ar, err)
+	}
+
+	_, res, err := w.validator.Validate(ctx, obj)
+	if err != nil {
+		return w.toAdmissionErrorResponse(ar, err)
+	}
+
+	// Forge response.
+	return &admissionv1beta1.AdmissionResponse{
+		UID:     ar.Request.UID,
+		Allowed: res.Valid,
+		Result: &metav1.Status{
+			Status:  metav1.StatusSuccess,
+			Message: res.Message,
+		},
+	}
+}
+
+func (w *staticWebhook) toAdmissionErrorResponse(ar *admissionv1beta1.AdmissionReview, err error) *admissionv1beta1.AdmissionResponse {
+	return helpers.ToAdmissionErrorResponse(ar.Request.UID, err, w.logger)
+}
